2067ee7ba71550975d34087635e669dab326de43
jpaola <jpaola@users.noreply.github.com>
2023-06-30 17:57:07 -0400

    Adding Firebase to project

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import MemoryGame from './components/MemoryGame'
-import './styles/index.scss'
-import 'antd/dist/antd.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import MemoryGame from "./components/MemoryGame";
+import "./styles/index.scss";
+import "antd/dist/antd.css";
+import {initializeApp} from "firebase/app";
 
-ReactDOM.render(
-  <MemoryGame />,
-  document.getElementById('root')
-);
+initializeApp({
+	apiKey: "AIzaSyD7EjSE_gijEHbS28CtMhBJr1wZ4xbg0nU",
+	authDomain: "memory-card-game-336b0.firebaseapp.com",
+	projectId: "memory-card-game-336b0",
+	storageBucket: "memory-card-game-336b0.appspot.com",
+	messagingSenderId: "686260334968",
+	appId: "1:686260334968:web:457627c8c1bf3212ba588d",
+	measurementId: "G-3L033XTQB3",
+});
+
+ReactDOM.render(<MemoryGame />, document.getElementById("root"));
